037875fcbf631afc9fe188e7212d0843c89212d6
Umida <mkh.umida@gmail.com>
2023-03-07 10:46:43 -0800

    initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+        <div className='loadingSpinnerContainer'>
+            <div className='loadingSpinnerContainer'></div>
+        </div>
+    );
+};
+
+export default Spinner;
